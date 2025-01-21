06dafe80432e85c3befe6bcfa06b766cfb8db4ab
Derian Alexis Flores Perez <125107629+Alexis260696@users.noreply.github.com>
2025-01-21 12:55:33 -0600

    Actualizar 01-Hello_world.js

---
+++ 01-Hello_world.js
@@ -0,0 +1,3 @@
+# descargar lo necesario para ejecutar javascript en vscode
+# imprimir un hello world en consola en este mismo archivo 
+# crear una carpeta en el proyecto que se llame 01_Tipos_básicos y crear dentro un archivo llamado 01-Variables
