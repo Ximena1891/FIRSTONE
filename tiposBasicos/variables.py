928d5edea10397b0dd881e43455e50dd8d25abc9
alexis <alexian260696@gmail.com>
2025-01-25 10:53:40 -0600

    datos basicos

---
+++ tiposBasicos/variables.py
@@ -0,0 +1,12 @@
+# una variable es un tipo de dato que puede cambiar su valor 
+
+string = "hola soy una variable" 
+number = 12
+float = 1.5
+boolean0 = False
+boolean1 = True
+
+string += " que ah cambiado"
+
+number += 5
+print(number)
