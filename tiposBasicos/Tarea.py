490366b06179b6520f20c73796f998980d2db505
Ximena1891 <ximena.tammy@gmail.com>
2025-01-25 14:38:35 -0600

    Tarea descabellada

---
+++ tiposBasicos/Tarea.py
@@ -0,0 +1,9 @@
+x = int(input("escribe tu edad "))     
+
+if x < 18  : 
+    print("si x es menor que 18 es menor de edad")
+elif x > 18 :
+    print("si x es mayor que 18 es mayor de edad")     
+else:
+    print("el numero que escribiste no es mayor ni menor que 18" )
+
