54d8fdebf4fe943afc24c7418c0c99fbfd2d35d3
alexis <alexian260696@gmail.com>
2025-01-25 10:23:29 -0600

    python

--- 01-Hello_world.js
+++ 01-Hello_world.js
@@ -1,4 +1,3 @@
 
-// imprimir un hello world en consola en este mismo archivo
-console.log("Hello world!"); 
-// crear una carpeta en el proyecto que se llame 01_Tipos_básicos y crear dentro un archivo llamado 01-Variables
+// imprimir un hello world en terminal en este mismo archivo
+console.log("Hello world desde JS");
