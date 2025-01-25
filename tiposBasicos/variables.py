d0f05b16e50ee211235e21a331103f8caa18bb04
alexis <alexian260696@gmail.com>
2025-01-25 12:13:38 -0600

    tarea 1

--- tiposBasicos/variables.py
+++ tiposBasicos/variables.py
@@ -1,12 +1,35 @@
 # una variable es un tipo de dato que puede cambiar su valor 
 
-string = "hola soy una variable" 
-number = 12
-float = 1.5
-boolean0 = False
-boolean1 = True
+# string = "hola soy una variable" 
+# integer = 12
+# float = 1.5
+# boolean0 = True
+# boolean1 = True
 
-string += " que ah cambiado"
+x = int(input("escribe un numero para comparar con el numero 5 y el 3 "))
 
-number += 5
-print(number)
+if x < 3 :
+    print("tu numero es menor que 3")
+elif x > 5 :
+    print ("tu numero es mayor que 5")     
+else:
+    print("el numero que escribiste no es mayor ni menor que 5")
+
+
+# string += " que ah cambiado"
+
+
+# while boolean1 == True:
+
+#     if boolean0 == True :
+#         print(" exito boolean0 ahora es true")
+#         boolean0 = False
+#     else :
+#         print(" boolean0 no es true , es false")
+#         boolean1 = False
+
+# print(" eh terminado")
+
+# investigar una pagina que se llama W3school python 
+
+# hacer un comparador que me diga cuando el usuario es mayor de edad 
