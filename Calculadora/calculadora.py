e3eb289fa20fefc511495e56108f423e7e3ca881
Ximena1891 <ximena.tammy@gmail.com>
2025-01-30 13:27:32 -0600

    lalala

---
+++ Calculadora/calculadora.py
@@ -0,0 +1,7 @@
+print("Bienvenido a la calculadora de Ximux")
+print("selecciona una opcion")
+print("abrir calculadora (1)\ncerrar calculadora (2)")
+print("")
+respuesta = input("R=")
+if respuesta != int(respuesta):
+    print("la respuesta tiene ue ser un numero")
